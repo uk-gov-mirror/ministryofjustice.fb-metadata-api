84562079096c6acf1be45bd48db10a00974c002b
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-10-19 11:34:46 +0100

    First baisc implementation of get latest metadata

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,11 @@
 class ServicesController < ApplicationController
+  def show
+    service = Service.find(params[:id])
+    metadata = service.metadata.order(created_at: :desc).first
+
+    render json: MetadataSerialiser.new(service, metadata).attributes, status: :ok
+  end
+
   def create
     service = Service.new(service_params)
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,10 @@ FactoryBot.define do
     name { 'Cowboy Bebop' }
     created_by  { 'Fay' }
   end
+
+  factory :metadata do
+    data { { configuration: {}, pages: []  } }
+    created_by { 'Fay' }
+    locale { 'en' }
+  end
 end
