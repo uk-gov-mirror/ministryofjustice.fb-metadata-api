a5bf41b8fcb2d43dc82235e6fcac89287fd8b43e
brenetic <brendan.butler@digital.justice.gov.uk>
2020-12-02 22:39:51 +0000

    Preload schemas into JSON::Validator
    Local referencing seems to be an issue with the json-schema gem. There
    are a number of issues open:
    
    https://github.com/ruby-json-schema/json-schema/issues/190
    https://github.com/ruby-json-schema/json-schema/issues/370
    https://github.com/ruby-json-schema/json-schema/issues/411
    
    To get around this we are preloading all the schemas into the
    JSON::Validator at startup using the _name property as the identifier
    for each schema. This should allow the JSON::Validator the ability to
    more easily find the necessary referenced schemas.

--- app/filters/validate_schema.rb
+++ app/filters/validate_schema.rb
@@ -16,16 +16,22 @@ class ValidateSchema
     end
 
     def validate(metadata, schema_name)
-      schema = Rails.application.config.schemas[schema_name] ||= find(schema_name)
+      schema = JSON::Validator.schema_for_uri(schema_name)&.schema || find(schema_name)
       JSON::Validator.validate!(schema, metadata)
     end
 
     def find(schema_name)
       schema_file = schema_name.gsub('.', '/')
-      schema = File.read(File.join(
-        Rails.application.config.schemas_directory, "#{schema_file}.json")
+      schema = JSON.parse(
+        File.read(
+          File.join(
+            Rails.application.config.schemas_directory, "#{schema_file}.json"
+          )
+        )
       )
-      JSON.parse(schema)
+      jschema = JSON::Schema.new(schema, Addressable::URI.parse(schema['_name']))
+      JSON::Validator.add_schema(jschema)
+      JSON::Validator.schema_for_uri(schema_name).schema
     rescue Errno::ENOENT
       raise SchemaNotFoundError.new("Schema not found => #{schema_name}")
     end
