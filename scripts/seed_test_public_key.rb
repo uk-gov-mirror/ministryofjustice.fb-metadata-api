602d6be416c2d4e2d6694b309cd8a78a23a1db68
Natalie Seeto <njseeto@users.noreply.github.com>
2021-01-07 12:38:59 +0000

    Change scripts to integrate the editor with the metadata api
    Add the private/public key pair to the repo
    
    In order to integrate with the metadata api we need to have the
    private key in the client apps and the public key in the
    redis for the service token cache app to find the public key for the
    issuer (fb-editor as example).
    
    Co-authored-by: Natalie Seeto <natalie.seeto@digital.justice.gov.uk>
    Co-authored-by: Brendan Butler <brendan.butler@digital.justice.gov.uk>
    
    Add the public keys to redis container every time
    
    Add setup make task
    
    This task will use the private/public key in the fixtures and build
    the containers

--- scripts/seed_test_public_key.rb
+++ scripts/seed_test_public_key.rb
@@ -1,14 +1,39 @@
 require 'openssl'
 require 'base64'
 
-puts 'Generating test private key'
-private_key = OpenSSL::PKey::RSA.generate(2048)
-File.open('./spec/fixtures/private.pem', 'w') { |file| file.write(private_key.to_pem) }
+private_key_file = './spec/fixtures/private.pem'
+public_key_file = './spec/fixtures/public.pem'
 
-puts 'Writing public key'
-public_key = private_key.public_key.to_pem
-File.open('./spec/fixtures/public.pem', 'w') { |file| file.write(public_key) }
+def add_public_key_to_redis(public_key_file)
+  public_key = File.read(public_key_file)
+  encoded_public_key = Base64.strict_encode64(public_key)
 
-puts 'Putting public key in Redis'
-encoded_public_key = Base64.strict_encode64(public_key)
-`docker-compose exec metadata-app-service-token-cache-redis redis-cli set 'encoded-public-key-integration-tests' #{encoded_public_key}`
+  puts "Adding the key 'encoded-public-key-fb-integration-tests' in Redis."
+  `docker-compose exec metadata-app-service-token-cache-redis redis-cli set 'encoded-public-key-integration-tests' #{encoded_public_key}`
+
+  puts "======= encoded-public-key-integration-tests ======"
+  puts `docker-compose exec metadata-app-service-token-cache-redis redis-cli get 'encoded-public-key-integration-tests'`
+  puts
+
+  puts "Adding the key 'encoded-public-key-fb-editor' in Redis."
+  `docker-compose exec metadata-app-service-token-cache-redis redis-cli set 'encoded-public-key-fb-editor' #{encoded_public_key}`
+
+  puts "======= Value in Redis of the key encoded-public-key-fb-editor ========"
+  puts `docker-compose exec metadata-app-service-token-cache-redis redis-cli get 'encoded-public-key-fb-editor'`
+  puts
+end
+
+
+if File.exist?(private_key_file) && File.exist?(public_key_file)
+  add_public_key_to_redis(public_key_file)
+  exit(0)
+else
+  puts 'Generating test private key'
+  private_key = OpenSSL::PKey::RSA.generate(2048)
+  File.open(private_key_file, 'w') { |file| file.write(private_key.to_pem) }
+
+  puts 'Writing public key'
+  public_key = private_key.public_key.to_pem
+  File.open(public_key_file, 'w') { |file| file.write(public_key) }
+  add_public_key_to_redis(public_key_file)
+end
